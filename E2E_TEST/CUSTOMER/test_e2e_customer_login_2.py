85352e24cc6e5e3e444454d263dfbc86b988f491
Michael Alem <salazaralem@gmail.com>
2024-03-03 09:39:55 +0300

    added stripe checkout

--- E2E_TEST/CUSTOMER/test_e2e_customer_login_2.py
+++ E2E_TEST/CUSTOMER/test_e2e_customer_login_2.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 @pytest.fixture(scope="class")
@@ -19,7 +20,7 @@ def browser():
 @pytest.fixture(scope="class")
 def logged_in_browser(browser):
     browser.get("https://dev-np.netryde.com/")
-    browser.set_window_size(1366, 728)
+    browser.maximize_window()
     browser.find_element(By.ID, "Log inTextArea").click()
     browser.find_element(By.LINK_TEXT, "Login as a Customer").click()
     browser.find_element(By.ID, "1").send_keys("nrct1@zprodev.com")
@@ -32,28 +33,30 @@ def logged_in_browser(browser):
 
 class TestE2ECustomerLogin:
     def test_e2e_customer_login(self, logged_in_browser):
-        time.sleep(5)
+        time.sleep(3)
         browser = logged_in_browser 
         assert 'https://dev-np.netryde.com/dashboard/map' in browser.current_url
 
-    # def test_e2e_get_price(self,logged_in_browser):
-    #     browser=logged_in_browser
-    #     browser.get("https://dev-np.netryde.com/")
-    #     browser.set_window_size(1218, 727)
-    #     browser.find_element(By.CSS_SELECTOR, ".point-a").click()
-    #     browser.find_element(By.CSS_SELECTOR, ".point-a").send_keys("Washington Heights,Washington, Georgia,United States")
-    #     time.sleep(2)
-    #     browser.find_element(By.CSS_SELECTOR, ".point-a").click()
-    #     browser.find_element(By.CSS_SELECTOR, ".point-b").send_keys("Washington Heights,Vermont, United States")
-    #     time.sleep(5)
-    #     browser.find_element(By.CSS_SELECTOR, ".point-b").send_keys(Keys.ENTER)
-    #     time.sleep(2)
-    #     browser.find_element(By.XPATH, "//button[@id='Get PriceTextArea']").click()
-    #     wait = WebDriverWait(self.driver, 10)
-    #     element = wait.until(EC.visibility_of_element_located((By.XPATH, "xpath=//div[@id='root']/div/div/div/div/div/div[2]/div/div/div/div[2]/button")))
-    #     element.click()
+    def test_e2e_get_price(self,logged_in_browser):
+        browser=logged_in_browser
+        browser.get("https://dev-np.netryde.com/")
+        browser.find_element(By.CSS_SELECTOR, ".point-a").click()
+        browser.find_element(By.CSS_SELECTOR, ".point-a").send_keys("2700 16th Avenue, Edgewood,")
+        time.sleep(2)
+        browser.find_element(By.CSS_SELECTOR, ".suggestion:nth-child(1)").click()
 
-    #     assert 'https://dev-np.netryde.com/dashboard/map' in browser.current_url
+        browser.find_element(By.CSS_SELECTOR, ".point-b").click()
+        browser.find_element(By.CSS_SELECTOR, ".point-b").send_keys("8407 Rainier Avenue South, Seattle")
+        time.sleep(5)
+        browser.find_element(By.CSS_SELECTOR, ".suggestion:nth-child(1)").click()
+        time.sleep(2)
+        browser.find_element(By.ID, "Get PriceTextArea").click()
+        time.sleep(5)
+        element = browser.find_element(By.TAG_NAME, "button")
+        element.click()
+        time.sleep(5)
+
+        assert 'https://dev-np.netryde.com/dashboard/map' in browser.current_url
 
 
     def test_booking(self,logged_in_browser):
@@ -63,19 +66,18 @@ class TestE2ECustomerLogin:
         #browser.find_element(By.CSS_SELECTOR, ".point-b").send_keys(Keys.ENTER)
         
 
-        browser.find_element(By.CSS_SELECTOR, ".point-a").click()
-        browser.find_element(By.CSS_SELECTOR, ".point-a").clear()
-        browser.find_element(By.CSS_SELECTOR, ".point-a").send_keys("8407 Rainier Avenue South, Seattle, Washington 98118, United States")
-        time.sleep(3)
-        browser.find_element(By.CSS_SELECTOR, ".suggestion:nth-child(1)").click()
-        #browser.find_element(By.CSS_SELECTOR, ".point-a").send_keys(Keys.ENTER)
+        # browser.find_element(By.CSS_SELECTOR, ".point-a").click()
+        # browser.find_element(By.CSS_SELECTOR, ".point-a").clear()
+        # browser.find_element(By.CSS_SELECTOR, ".point-a").send_keys("8407 Rainier Avenue South, Seattle, Washington 98118, United States")
+        # time.sleep(3)
+        # browser.find_element(By.CSS_SELECTOR, ".suggestion:nth-child(1)").click()
+        # #browser.find_element(By.CSS_SELECTOR, ".point-a").send_keys(Keys.ENTER)
 
         
-        browser.find_element(By.CSS_SELECTOR, ".point-b").click()
-        browser.find_element(By.CSS_SELECTOR, ".point-b").clear()
-        browser.find_element(By.CSS_SELECTOR, ".point-b").send_keys("2700 16th Avenue, Edgewood, Washington 98354, United States")
-        time.sleep(5)
-        browser.find_element(By.CSS_SELECTOR, ".suggestion:nth-child(1)").click() 
+        # browser.find_element(By.CSS_SELECTOR, ".point-b").click()
+        # browser.find_element(By.CSS_SELECTOR, ".point-b").send_keys("2700 16th Avenue, Edgewood, Washington 98354, United States")
+        # time.sleep(5)
+        # browser.find_element(By.CSS_SELECTOR, ".suggestion:nth-child(1)").click() 
 
 
 
@@ -89,27 +91,47 @@ class TestE2ECustomerLogin:
 
        
         date_input.send_keys(next_day)
-
-        
         time_input.send_keys(six_hour)
 
         time.sleep(3)
+        browser.execute_script("window.scrollTo(0, 0);")
         browser.find_element(By.CSS_SELECTOR, "label:nth-child(10)").click()
+        browser.find_element(By.XPATH, "//div[@id='root']/div/div/div[2]/div[9]/button").click()
         
         
+        time.sleep(10)
+        assert 'https://checkout.stripe.com' in browser.current_url
+
+    def test_stripe_checkout(self,logged_in_browser):
+        browser=logged_in_browser
+        browser.find_element(By.ID, "cardNumber").click()
         
-        time.sleep(20)
         
-        assert True
+        browser.find_element(By.ID, "cardNumber").send_keys("4242 4242 4242 4242")
+        
+        browser.find_element(By.ID, "cardExpiry").send_keys("04 / 24")
+        
+        browser.find_element(By.ID, "cardCvc").send_keys("123")
+
+        browser.find_element(By.ID, "billingName").send_keys("Michael Alem")
+        
+        time.sleep(2)
+        browser.find_element(By.CSS_SELECTOR, ".SubmitButton-IconContainer").click()
+        time.sleep(8)
+        browser.find_element(By.CSS_SELECTOR, ".\\_booking-status-container_5f60k_426").click()
+        time.sleep(5)
+        assert browser.find_element(By.CSS_SELECTOR, ".\\_booking-status-item_5f60k_437:nth-child(1) > p").text == "Trip Booked"
+        browser.find_element(By.LINK_TEXT, "Book another trip").click()
 
 
-    # def test_e2e_customer_logout(self, logged_in_browser):
-    #     browser = logged_in_browser
-    #     time.sleep(3)
-    #     browser.find_element(By.CSS_SELECTOR, ".\\_nav-links_1xe3h_48 h1").click()
-    #     logout_btn=browser.find_element(By.CSS_SELECTOR, ".\\_user-menu-logout-btn_1xe3h_219")
-    #     logout_btn.click()
-    #     assert 'NetRyde' in browser.title
+    def test_e2e_customer_logout(self, logged_in_browser):
+        browser = logged_in_browser
+        #browser.get("https://dev-np.netryde.com/")
+        time.sleep(3)
+        browser.find_element(By.CSS_SELECTOR, ".\\_nav-links_1xe3h_48 h1").click()
+        logout_btn=browser.find_element(By.CSS_SELECTOR, ".\\_user-menu-logout-btn_1xe3h_219")
+        logout_btn.click()
+        assert 'NetRyde' in browser.title
 
 
 
